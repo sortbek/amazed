96786752835ee94fd46ad0383a270933f13b2164
jeffrey wienen <jeffreywienen@jeffreys-MacBook-Pro.local>
2017-04-18 15:55:45 +0200

    Fixed a bunch of stuff Integrated heap

--- Assets/Scripts/HelloWorld.cs
+++ Assets/Scripts/HelloWorld.cs
@@ -1,16 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class HelloWorld : MonoBehaviour {
+public class HelloWorld : MonoBehaviour
+{
+
+    private int i;
+
+    private Text healthText;
+    private Text staminaText;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+	{
 	}
 }
