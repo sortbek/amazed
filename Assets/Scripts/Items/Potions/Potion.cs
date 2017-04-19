ff291d4d136c99fbc72c236fa2973d069eedb2e4
Hugo Kamps <hugokamps6@hotmail.com>
2017-04-19 10:56:56 +0200

    HUD - Potion selection and statsupdater (health and stamina)

---
+++ Assets/Scripts/Items/Potions/Potion.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Items.Potions {
+    public abstract class Potion {
+        public int Amount;
+        public Texture Texture;
+
+        public Potion()
+        {
+            Amount = 5;
+        }
+
+        public abstract void Use();
+    }
+}
---
+++ Assets/Scripts/Items/Potions/SpeedPotion.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Items.Potions
+{
+    class SpeedPotion : Potion
+    {
+        public SpeedPotion()
+        {
+            Texture = (Texture)Resources.Load("Sprites/potion_speed", typeof(Texture));
+        }
+        public override void Use()
+        {
+            // Increase speed for a limited time
+            Amount -= 1;
+        }
+    }
+}
