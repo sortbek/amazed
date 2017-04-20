8846344cafb6de509c820944d18b8a1d33aa9644
JordiW <wolthuis.jordi@gmail.com>
2017-04-20 14:22:42 +0200

    changed variables

--- Assets/Scripts/MainMenu/MenuBehaviour.cs
+++ Assets/Scripts/MainMenu/MenuBehaviour.cs
@@ -6,74 +6,63 @@ using UnityEngine.SceneManagement;
 public class MenuBehaviour : MonoBehaviour
 {
 
-    public Canvas quitMenu;
-    public Canvas settingMenu;
-    public Canvas mainMenu;
-    public Button startText;
-    public Button exitText;
-    public Button settingText;
+    public Canvas QuitMenu;
+    public Canvas SettingMenu;
+    public Canvas MainMenu;
+    public Button StartText;
+    public Button ExitText;
+    public Button SettingText;
 
-    void Start()
-    {
-        mainMenu = mainMenu.GetComponent<Canvas>();
-        quitMenu = quitMenu.GetComponent<Canvas>();
-        settingMenu = settingMenu.GetComponent<Canvas>();
-        startText = startText.GetComponent<Button>();
-        exitText = exitText.GetComponent<Button>();
-        settingText = settingText.GetComponent<Button>();
-        quitMenu.enabled = false;
-        settingMenu.enabled = false;
+    void Start() {
+        MainMenu = MainMenu.GetComponent<Canvas>();
+        QuitMenu = QuitMenu.GetComponent<Canvas>();
+        SettingMenu = SettingMenu.GetComponent<Canvas>();
+        StartText = StartText.GetComponent<Button>();
+        ExitText = ExitText.GetComponent<Button>();
+        SettingText = SettingText.GetComponent<Button>();
+        QuitMenu.enabled = false;
+        SettingMenu.enabled = false;
     }
 
-    public void ExitPress()
-    {
+    public void ExitPress() {
         EnableExitMenu();
     }
 
-    public void SettingPress()
-    {
+    public void SettingPress() {
         EnableSettingsMenu();
     }
 
-    public void BackToMenuPress()
-    {
+    public void BackToMenuPress() {
         EnableMainMenu();
     }
 
-    public void StartLevel()
-    {
+    public void StartLevel() {
         SceneManager.LoadScene(1); //this will load our first level from our build settings. "1" is the second scene in our game
-
     }
 
-    public void ExitGame()
-    {
+    public void ExitGame() {
         Application.Quit();
-
     }
 
-    public void EnableMainMenu()
-    {
-        mainMenu.enabled = true;
-        startText.enabled = true;
-        exitText.enabled = true;
-        settingText.enabled = true;
-        settingMenu.enabled = false;
-        quitMenu.enabled = false;
+    public void EnableMainMenu() {
+        MainMenu.enabled = true;
+        StartText.enabled = true;
+        ExitText.enabled = true;
+        SettingText.enabled = true;
+        SettingMenu.enabled = false;
+        QuitMenu.enabled = false;
     }
 
-    public void EnableSettingsMenu()
-    {
-        settingMenu.enabled = true;
-        mainMenu.enabled = false;
-        quitMenu.enabled = false;
+    public void EnableSettingsMenu() {
+        SettingMenu.enabled = true;
+        MainMenu.enabled = false;
+        QuitMenu.enabled = false;
     }
 
-    public void EnableExitMenu()
-    {
-        quitMenu.enabled = true;
-        settingMenu.enabled = false;
-        mainMenu.enabled = false;
+    public void EnableExitMenu() {
+        QuitMenu.enabled = true;
+        SettingMenu.enabled = false;
+        MainMenu.enabled = false;
     }
 
 }
