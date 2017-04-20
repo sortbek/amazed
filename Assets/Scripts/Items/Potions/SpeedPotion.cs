4486d9fdce52610cfe53c4e3db37ce567f657bb3
Hugo Kamps <hugokamps6@hotmail.com>
2017-04-20 15:06:00 +0200

    Error fixes

--- Assets/Scripts/Items/Potions/SpeedPotion.cs
+++ Assets/Scripts/Items/Potions/SpeedPotion.cs
@@ -9,10 +9,6 @@ namespace Assets.Scripts.Items.Potions {
 
         public override void Use() {
             Player.Speed += Boost;
-
-            PotionRunnable pr = new PotionRunnable(Player, this, 4000);
-            pr.Start();
-
             base.Use();
         }
 
