d1a84ca792cd095740a7c6494199c263ad351993
JordiW <wolthuis.jordi@gmail.com>
2017-05-03 15:24:57 +0200

    Main-menu-settings
    Added setting functionality

---
+++ Assets/Scripts/MainMenu/GameSettings.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSettings {
+
+    private bool fullscreen;
+    private int textureQuality;
+    private int antialiasing;
+    private int vSync;
+    private int resolutionIndex;
+    private float musicVolume;
+
+    public bool Fullscreen
+    {
+        get
+        {
+            return fullscreen;
+        }
+
+        set
+        {
+            fullscreen = value;
+        }
+    }
+
+    public int TextureQuality
+    {
+        get
+        {
+            return textureQuality;
+        }
+
+        set
+        {
+            textureQuality = value;
+        }
+    }
+
+    public int Antialiasing
+    {
+        get
+        {
+            return antialiasing;
+        }
+
+        set
+        {
+            antialiasing = value;
+        }
+    }
+
+    public int VSync
+    {
+        get
+        {
+            return vSync;
+        }
+
+        set
+        {
+            vSync = value;
+        }
+    }
+
+    public int ResolutionIndex
+    {
+        get
+        {
+            return resolutionIndex;
+        }
+
+        set
+        {
+            resolutionIndex = value;
+        }
+    }
+
+    public float MusicVolume
+    {
+        get
+        {
+            return musicVolume;
+        }
+
+        set
+        {
+            musicVolume = value;
+        }
+    }
+}
