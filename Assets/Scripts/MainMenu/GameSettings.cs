6fbded7987c046d10bfaa26d7eb3f6960bfe2666
JordiW <wolthuis.jordi@gmail.com>
2017-05-04 13:34:01 +0200

    Settings functionality
    changes to settings

--- Assets/Scripts/MainMenu/GameSettings.cs
+++ Assets/Scripts/MainMenu/GameSettings.cs
@@ -4,88 +4,11 @@ using UnityEngine;
 
 public class GameSettings {
 
-    private bool fullscreen;
-    private int textureQuality;
-    private int antialiasing;
-    private int vSync;
-    private int resolutionIndex;
-    private float musicVolume;
+    public bool fullscreen;
+    public int textureQuality;
+    public int antialiasing;
+    public int vSync;
+    public int resolutionIndex;
+    public float musicVolume;
 
-    public bool Fullscreen
-    {
-        get
-        {
-            return fullscreen;
-        }
-
-        set
-        {
-            fullscreen = value;
-        }
-    }
-
-    public int TextureQuality
-    {
-        get
-        {
-            return textureQuality;
-        }
-
-        set
-        {
-            textureQuality = value;
-        }
-    }
-
-    public int Antialiasing
-    {
-        get
-        {
-            return antialiasing;
-        }
-
-        set
-        {
-            antialiasing = value;
-        }
-    }
-
-    public int VSync
-    {
-        get
-        {
-            return vSync;
-        }
-
-        set
-        {
-            vSync = value;
-        }
-    }
-
-    public int ResolutionIndex
-    {
-        get
-        {
-            return resolutionIndex;
-        }
-
-        set
-        {
-            resolutionIndex = value;
-        }
-    }
-
-    public float MusicVolume
-    {
-        get
-        {
-            return musicVolume;
-        }
-
-        set
-        {
-            musicVolume = value;
-        }
-    }
 }
