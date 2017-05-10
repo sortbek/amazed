e9494d7569304665a445a011ae28d47efe10460d
Hugo Kamps <hugokamps6@hotmail.com>
2017-05-10 23:15:27 +0200

    Points in transition screen

--- Assets/Scripts/Items/Potions/HealthPotion.cs
+++ Assets/Scripts/Items/Potions/HealthPotion.cs
@@ -9,7 +9,7 @@ namespace Assets.Scripts.Items.Potions {
         public override void Use() {
             if (Player.Health > 70) Player.Health = 100;
             else Player.Health += 30;
-
+            Player.Points += 90;
             base.Use();
         }
     }
