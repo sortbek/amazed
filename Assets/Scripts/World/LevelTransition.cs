1cdc0d6b41dbd0a7c08ae717c1dfb596e3cbb496
Hugo Kamps <hugokamps6@hotmail.com>
2017-05-10 23:15:27 +0200

    Points in transition screen

--- Assets/Scripts/World/LevelTransition.cs
+++ Assets/Scripts/World/LevelTransition.cs
@@ -3,17 +3,30 @@ using System.Collections;
 using Assets.Scripts;
 using Assets.Scripts.Character;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class LevelTransition : MonoBehaviour {
+public class LevelTransition : MonoBehaviour
+{
+    private Text _pointsAmount;
 
     // Use this for initialization
     void Start() {
+        foreach (var text in GetComponentsInChildren<Text>())
+        {
+            switch (text.name)
+            {
+                case "PointsAmount":
+                    _pointsAmount = text;
+                    break;
+            }
 
+        }
+        _pointsAmount.text = PlayerPrefs.GetInt("p").ToString();
     }
 
     // Update is called once per frame
-    void Update() {
-
+    void Update()
+    {
     }
 
     public void NextLevelClick()
