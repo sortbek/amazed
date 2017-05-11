90dd9190a67ec7bcb9f380aebb633775a2b73882
Hugo Kamps <hugokamps6@hotmail.com>
2017-05-11 10:16:13 +0200

    Time spent

--- Assets/Scripts/World/LevelTransition.cs
+++ Assets/Scripts/World/LevelTransition.cs
@@ -8,6 +8,7 @@ using UnityEngine.UI;
 public class LevelTransition : MonoBehaviour
 {
     private Text _pointsAmount;
+    private Text _timeAmount;
 
     // Use this for initialization
     void Start() {
@@ -18,10 +19,14 @@ public class LevelTransition : MonoBehaviour
                 case "PointsAmount":
                     _pointsAmount = text;
                     break;
+                case "TimeAmount":
+                    _timeAmount = text;
+                    break;
             }
 
         }
         _pointsAmount.text = PlayerPrefs.GetInt("p").ToString();
+        _timeAmount.text = PlayerPrefs.GetString("t");
     }
 
     // Update is called once per frame
