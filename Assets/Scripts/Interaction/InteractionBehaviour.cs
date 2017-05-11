6ec251707306116f877e080764cc68dedaf4e9cd
niekb553 <brink.niekvd@gmail.com>
2017-05-11 13:45:10 +0200

    Basic prop interaction

--- Assets/Scripts/Interaction/InteractionBehaviour.cs
+++ Assets/Scripts/Interaction/InteractionBehaviour.cs
@@ -4,9 +4,11 @@ using UnityEngine.UI;
 
 public class InteractionBehaviour : MonoBehaviour {
     private Text _interaction;
+    public bool HasBeenInteractedWith;
 
     // Use this for initialization
     void Start() {
+        HasBeenInteractedWith = false;
         _interaction = GameObject.FindGameObjectWithTag("interaction").GetComponent<Text>();
         // Radomly decide what item this prop holds
     }
@@ -16,7 +18,13 @@ public class InteractionBehaviour : MonoBehaviour {
     }
 
     public void Interact() {
-        if (_interaction != null) {
+        print("Im being interacted with :o");
+        HasBeenInteractedWith = true;
+        _interaction.text = "";
+    }
+
+    public void PossibleInteraction() {
+        if (_interaction != null && !HasBeenInteractedWith) {
             _interaction.text = String.Format("Press 'E' to search {0}", ToString());
         }
     }
