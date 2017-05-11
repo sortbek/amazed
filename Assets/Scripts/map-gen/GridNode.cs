a289dd208e06a0935167c1c35d4b884ede6f0935
Hugo Kamps <hugokamps6@hotmail.com>
2017-05-11 10:21:40 +0200

    removed changes gridnode

--- Assets/Scripts/map-gen/GridNode.cs
+++ Assets/Scripts/map-gen/GridNode.cs
@@ -23,23 +23,6 @@ namespace Assets.Scripts {
         public int Y;
         public int Key;
         public bool IsPartOfRoom;
-        private Generator _generator = Object.FindObjectOfType<Generator>();
-
-        public List<int> NoTop = new List<int> {
-            0, 2, 4, 6, 8, 10, 12, 14
-        };
-
-        public List<int> NoRight = new List<int> {
-            0, 1, 4, 5, 8, 9, 12, 13
-        };
-
-        public List<int> NoBottom = new List<int> {
-            0, 1, 2, 3, 8, 9, 10, 11
-        };
-
-        public List<int> NoLeft = new List<int> {
-            0, 1, 2, 3, 4, 5, 6, 7
-        };
 
         public List<GridNode> BakedList = new List<GridNode>();
         public GameObject Prefab;
@@ -59,26 +42,5 @@ namespace Assets.Scripts {
         }
 
         public int HeapIndex { get; set; }
-
-        public List<GridNode> GetAdjacentNodes() {
-
-            List<GridNode> adjacentNodes = new List<GridNode>();
-
-            if (_generator.IsGenerated()) {
-                if (NoTop.Contains(NodeConfiguration))
-                    adjacentNodes.Add(_generator.GridMap[X, Y - 1]);
-
-                if (NoRight.Contains(NodeConfiguration))
-                    adjacentNodes.Add(_generator.GridMap[X + 1, Y]);
-
-                if (NoBottom.Contains(NodeConfiguration))
-                    adjacentNodes.Add(_generator.GridMap[X, Y + 1]);
-
-                if (NoLeft.Contains(NodeConfiguration))
-                    adjacentNodes.Add(_generator.GridMap[X - 1, Y]);
-                return adjacentNodes;
-            }
-            return adjacentNodes;
-        }
     }
 }
