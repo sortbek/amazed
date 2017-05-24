40e26249370f7ac6a653f37729e56edd78656e6a
JordiW <wolthuis.jordi@gmail.com>
2017-05-24 11:06:55 +0200

    bug fix

--- Assets/Scripts/Util/DrawingUtil.cs
+++ Assets/Scripts/Util/DrawingUtil.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class DrawingUtil : MonoBehaviour {
     public Vector3 playerloc;
 
-    public const float speed = 4.5f;
+    public const float speed = 5f;
     public const float baseHeight = -1f;
     public const float particleHeight = 4f;
 
@@ -17,11 +17,6 @@ public class DrawingUtil : MonoBehaviour {
     public GameObject particle;
     public GameObject particleGO;
     public List<GameObject> particleClone = new List<GameObject>();
-
-    public void spawnGuidancePixie(Vector3[] path, Vector3 playerloc) {
-       // StartCoroutine(spawnDust(path, playerloc, 10));
-    }
-
 
     private void Update() {
         if (_path == null || _path.Count == 0 || _path.Count <= nodeIndex)
@@ -45,11 +40,9 @@ public class DrawingUtil : MonoBehaviour {
 
     public void SpawnDust(Vector3[] path, Vector3 loc, int time) {
         _path = new List<Vector3>(path);
+        // inserts player location at the start of the list 
         _path.Insert(0, loc);
 
         particleGO = Instantiate(particle, loc, transform.rotation);
-
-     
-        //yield return new WaitForSeconds(time);
     }
 }
